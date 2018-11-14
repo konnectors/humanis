beb82852e57b9789d9cf2483786168da425aa3bc
doubleface <christophe@cozycloud.cc>
2018-11-14 18:02:32 +0100

    feat: humanis connector implementation

--- src/index.js
+++ src/index.js
@@ -3,127 +3,214 @@ const {
   requestFactory,
   signin,
   scrape,
+  saveFiles,
   saveBills,
+  cozyClient,
   log
 } = require('cozy-konnector-libs')
+const pdfjs = require('pdfjs-dist')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very useful for
-  // debugging but very verbose. That is why it is commented out by default
   // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
+const moment = require('moment')
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://sante-espaceparticuliers.humanis.com/'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
+  // first get the pdf reimbursemnts files and read references into it with pdfjs
+  log('info', 'Fetching pdf bills')
+  let files = await fetchFiles()
+  files = await saveFiles(files, fields, {
+    contentType: 'application/pdf'
+  })
+  files = await fetchRefsInPdf(files)
+
+  log('info', 'Fetching the list of documents')
+  const $ = await request(
+    `${baseUrl}e-releves-et-remboursements/vos-derniers-remboursements-sante`
+  )
+  log('info', 'Parsing list of documents')
+  let reimbursements = await parseReimbursements($)
+  const bills = []
+  for (let reimbursement of reimbursements) {
+    Array.prototype.push.apply(bills, await fetchDetails(reimbursement))
+  }
+
+  log('info', 'Linking documents with files')
+  linkBillsWithFiles(bills, files)
+
   log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
+  await saveBills(bills, fields, {
+    identifiers: ['humanis'],
+    keys: ['vendorRef']
   })
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if the login request was a success. Every website has
-    // different ways respond: http status code, error message in html ($), http redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be usefull for other connectors'
-      )
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
+async function linkBillsWithFiles(bills, files) {
+  bills.forEach(bill => {
+    const linkedFile = files.find(file => {
+      return file.refs.includes(bill.vendorRef)
+    })
+    if (linkedFile) {
+      bill.fileurl = linkedFile.fileurl
+      bill.filename = linkedFile.filename
+    } else {
+      log('warn', `Could not find linked file for ${bill.vendorRef}`)
+    }
+  })
+}
+
+async function fetchRefsInPdf(files) {
+  for (let file of files) {
+    const text = await getPdfText(file.fileDocument._id)
+    file.refs = text
+      .match(/([A-Z] \d{2} \d{3} \d{5})+/g)
+      .map(ref => ref.replace(/ /g, ''))
+  }
+  return files
+}
+
+async function getPdfText(fileId) {
+  const response = await cozyClient.files.downloadById(fileId)
+  const buffer = await response.buffer()
+  const document = await pdfjs.getDocument(buffer)
+  const page = await document.getPage(1)
+  return (await page.getTextContent()).items.map(doc => doc.str).join(' ')
+}
+
+async function fetchFiles() {
+  const $ = await request(
+    baseUrl + 'e-releves-et-remboursements/vos-e-releves-sante'
+  )
+  moment.locale('fr')
+  return scrape(
+    $,
+    {
+      date: {
+        sel: '.date .text',
+        parse: date => moment(date, 'MMMM YYYY')
+      },
+      fileurl: {
+        sel: '.download-pdf-document',
+        attr: 'href',
+        parse: href => baseUrl + href
+      },
+      amount: {
+        sel: '.amount .text',
+        parse: amount => parseFloat(amount.replace(' €', ''))
       }
+    },
+    '.estatement-row'
+  )
+    .filter(doc => doc.amount !== '0.00 €')
+    .map(doc => {
+      doc.filename = `Humanis_${doc.date.format('YYYY_MM').replace('.', ',')}_${
+        doc.amount
+      }€.pdf`
+      delete doc.date
+      delete doc.amount
+      return doc
+    })
+}
+
+async function fetchDetails(doc) {
+  const $ = await request(baseUrl + doc._detailsUrl)
+  delete doc._detailsUrl
+  doc.beneficiary = $('.group-name')
+    .text()
+    .trim()
+    .split(' ')
+    .slice(1)
+    .join(' ')
+
+  const details = scrape(
+    $,
+    {
+      subtype: '.categories .text',
+      originalDate: {
+        sel: '.date-des-soins .text',
+        parse: date => moment(date, 'DD/MM/YYYY').toDate()
+      },
+      originalAmount: {
+        sel: '.depenses .text',
+        parse: amount => parseFloat(amount.trim().replace(' €', ''))
+      },
+      socialSecurityRefund: {
+        sel: '.remboursement-secu .text',
+        parse: amount => parseFloat(amount.trim().replace(' €', ''))
+      },
+      amount: {
+        sel: '.remboursement-notre .text',
+        parse: amount => parseFloat(amount.trim().replace(' €', ''))
+      }
+    },
+    '.transfer-details-row'
+  )
+
+  return details.map(detail => ({
+    ...doc,
+    ...detail,
+    isRefund: true,
+    currency: 'EUR',
+    metadata: {
+      importDate: new Date(),
+      version: 1
+    }
+  }))
+}
+
+function authenticate(name, pass) {
+  return signin({
+    url: baseUrl,
+    formSelector: 'form',
+    formData: { name, pass },
+    validate: (statusCode, $) => {
+      const errors = $('.messages--error')
+      if (errors.length) {
+        log('error', errors.text())
+        return false
+      } else return true
     }
   })
 }
 
 // The goal of this function is to parse a html page wrapped by a cheerio instance
 // and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
+function parseReimbursements($) {
+  return Array.from($('.transfer-row'))
+    .map(row => {
+      const $row = $(row)
+      return {
+        date: moment(
+          $row
+            .find('.date')
+            .text()
+            .trim(),
+          'DD/MM/YY'
+        ).toDate(),
+        groupAmount: parseFloat(
+          $row
+            .find('.montant span.text')
+            .text()
+            .trim()
+            .replace(' €', '')
+        ),
+        vendorRef: $row
+          .find('.dossier span.text')
+          .text()
+          .trim(),
+        _detailsUrl: $row.find('.details a').attr('href'),
+        vendor: 'Humanis'
       }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
-      importDate: new Date(),
-      // document version, useful for migration after change of document structure
-      version: 1
-    }
-  }))
-}
-
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+    })
+    .filter(doc => doc.groupAmount !== 0)
 }
