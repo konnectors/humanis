2c41abc26b0026052a748285b4188b2ffe79de0a
doubleface <christophe@cozycloud.cc>
2018-11-16 18:22:57 +0100

    fix: add the amount back in the filename

--- src/index.js
+++ src/index.js
@@ -117,9 +117,9 @@ async function fetchFiles() {
   )
     .filter(doc => doc.amount !== '0.00 €')
     .map(doc => {
-      doc.filename = `${doc.date
-        .format('YYYY_MM')
-        .replace('.', ',')}_Humanis.pdf`
+      doc.filename = `${doc.date.format('YYYY_MM').replace('.', ',')}_${
+        doc.amount
+      }€_Humanis.pdf`
       delete doc.date
       delete doc.amount
       return doc
